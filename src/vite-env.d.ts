b1ad71f96ab22909dcbb6c543186f89ac0a935d8
dev-mkr <webdev.mu@gamil.com>
2022-10-13 01:32:58 +0200

    Initial commit 🎉

---
+++ src/vite-env.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+// the line above for svg as component error see https://github.com/pd4d10/vite-plugin-svgr#readme

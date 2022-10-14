6def8a71d8a9e70ca13bc108a41a847be47334ee
dev-mkr <webdev.mu@gamil.com>
2022-10-15 00:49:43 +0200

    finished rightSide section 🎉

---
+++ src/Layout/RightSide/index.tsx
@@ -0,0 +1,15 @@
+import SearchBar from "./components/SearchBar";
+import WhatHappening from "./components/whatHappening";
+import WhoToFollow from "./components/whoToFollow";
+
+const index = () => {
+  return (
+    <div>
+      <SearchBar />
+      <WhatHappening />
+      <WhoToFollow />
+    </div>
+  );
+};
+
+export default index;
